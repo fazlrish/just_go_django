33fc8851ccdbb7babf3162f6d828ec083ba0c325
Rishat Fazlytdinov <r.fazlytdinov@corp.mail.ru>
2017-12-21 12:43:55 +0300

    Base

--- event/models.py
+++ event/models.py
@@ -1,11 +1,26 @@
 # Create your models here.
 from django.db import models
+from django.db.models import ManyToManyField
 from django.utils import timezone
+
+
+#ToDo Look at time zone
+from authentication.models import BaseUser
 
 
 class Event(models.Model):
 
-    dateTime = models.DateTimeField(blank=False, null=False, default=timezone.now())
-    eventType = models.PositiveSmallIntegerField(blank=False, null=False)
+    user = ManyToManyField(BaseUser)
+    name = models.CharField(blank=False, null=False, max_length=128)
+    describe = models.TextField(max_length=600)
+    type = models.PositiveSmallIntegerField(blank=False, null=False)
+    status = models.PositiveSmallIntegerField(blank=False, null=False)
+    start_time = models.DateTimeField(blank=False, null=False, default=timezone.now())
+    end_time = models.DateTimeField(blank=False, null=False, default=timezone.now())
 
+class Place(models.Model):
 
+    name = models.CharField(blank=False, null=False, max_length=128)
+    lat = models.FloatField(blank=False, null=False)
+    lng = models.FloatField(blank=False, null=False)
+    event = models.ManyToManyField(Event)
